e3310deea88c99e9799b61eb08c38914fcd88d04
mariana-schlick <mariana.schlick@gmail.com>
2024-12-16 16:23:18 -0300

    Arquivos criados pelo Spring Initializr

---
+++ src/main/java/br/com/marianaschlick/desafiojavajr/DesafiojavajrApplication.java
@@ -0,0 +1,13 @@
+package br.com.marianaschlick.desafiojavajr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiojavajrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiojavajrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marianaschlick/desafiojavajr/DesafiojavajrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marianaschlick.desafiojavajr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiojavajrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
